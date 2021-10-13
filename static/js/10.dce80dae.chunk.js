6aa9c752706159320b8adad37020e19bbe192e0c
termorey <nickfierydragon1995@mail.ru>
2021-10-14 02:44:16 +0300

    Delete static directory

--- static/js/10.dce80dae.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpevent=this.webpackJsonpevent||[]).push([[10],{39:function(e,t,a){"use strict";a.r(t),t.default=a.p+"static/media/Frame.6da921ac.png"}}]);
-//# sourceMappingURL=10.dce80dae.chunk.js.map
