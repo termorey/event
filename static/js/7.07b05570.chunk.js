34fefe908acbedd65315a65e03b7d7aca7697859
termorey <nickfierydragon1995@mail.ru>
2021-10-17 19:13:18 +0300

    Update

--- static/js/7.07b05570.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpevent=this.webpackJsonpevent||[]).push([[7],{36:function(e,n,t){e.exports={content:"event1_content__Nmm5f",wrapper:"event1_wrapper__QnViW",cardsList:"event1_cardsList__3mLMf",answersList:"event1_answersList__21KE6",controls:"event1_controls__2Bm9d",sound:"event1_sound__29R7m"}}}]);
-//# sourceMappingURL=7.07b05570.chunk.js.map
