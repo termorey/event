721364259a9c8c21da9c10b146438da26ce86ede
termorey <nickfierydragon1995@mail.ru>
2021-10-19 21:47:49 +0300

    Update

---
+++ static/js/12.612da578.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpevent=this.webpackJsonpevent||[]).push([[12],{36:function(e,n,t){e.exports={content:"event1_content__Nmm5f",wrapper:"event1_wrapper__QnViW",cardsList:"event1_cardsList__3mLMf",answersList:"event1_answersList__21KE6",controls:"event1_controls__2Bm9d",sound:"event1_sound__29R7m"}}}]);
+//# sourceMappingURL=12.612da578.chunk.js.map
---
+++ static/js/8.eec510c1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpevent=this.webpackJsonpevent||[]).push([[8],{39:function(e,t,a){"use strict";a.r(t),t.default=a.p+"static/media/Frame.6da921ac.png"}}]);
+//# sourceMappingURL=8.eec510c1.chunk.js.map
